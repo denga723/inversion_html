4d8d349480aa7800e6fd9d10c36edf8116af0f76
denga723 <denga723@newschool.edu>
2024-07-16 14:33:48 -0400

    hhh

--- include-bar.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    fetch('bar.html')
-        .then(response => response.text())
-        .then(data => {
-            document.getElementById('navbar').innerHTML = data;
-        })
-        .catch(error => console.error('Error loading navigation bar:', error));
-});
